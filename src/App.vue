d085c44e851087f6f3c760f08ea9f529e8893fec
CodeAkio <siqueira_2004@hotmail.com>
2021-11-17 19:12:59 -0300

    Add presentation organism

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="component">
-    <Header />
+    <Presentation />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { Header } from '@/components/organisms'
+import { Presentation } from '@/components/organisms'
 
 export default defineComponent({
   name: 'App',
-  components: { Header }
+  components: { Presentation }
 })
 </script>
 
