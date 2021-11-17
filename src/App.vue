96db4d3a75f5d9127e3c630ae9d4f584a5d56bf1
CodeAkio <siqueira_2004@hotmail.com>
2021-11-17 18:10:26 -0300

    Add header organism

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="component">
-    <Service
-      title="Front End"
-      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit fugit recusandae, possimus quaerat minima aliquid libero iusto nam qui vitae culpa doloribus aspernatur incidunt nemo quae pariatur id, sed architecto."
-    />
+    <Header />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { Service } from '@/components/molecules'
+import { Header } from '@/components/organisms'
 
 export default defineComponent({
   name: 'App',
-  components: { Service }
+  components: { Header }
 })
 </script>
 
