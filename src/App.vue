70e6100bf025cf154413278243929dc0678bc5a7
CodeAkio <siqueira_2004@hotmail.com>
2021-11-17 17:38:47 -0300

    Add social networks molecule

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="component">
-    <SocialLink name="Github" to="https://github.com" icon="github.svg" />
-    <SocialLink name="Twitter" to="https://twitter.com" icon="twitter.svg" />
-    <SocialLink name="Facebook" to="https://facebook.com" icon="facebook.svg" />
+    <SocialNetworks />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { SocialLink } from '@/components/atoms'
+import { SocialNetworks } from '@/components/molecules'
 
 export default defineComponent({
   name: 'App',
-  components: { SocialLink }
+  components: { SocialNetworks }
 })
 </script>
 
