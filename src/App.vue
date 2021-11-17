e97d3fba10f1dc09cafeb3251dbe67f4da61209f
CodeAkio <siqueira_2004@hotmail.com>
2021-11-17 17:49:59 -0300

    Add service molecule

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="component">
-    <SocialNetworks />
+    <Service
+      title="Front End"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit fugit recusandae, possimus quaerat minima aliquid libero iusto nam qui vitae culpa doloribus aspernatur incidunt nemo quae pariatur id, sed architecto."
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { SocialNetworks } from '@/components/molecules'
+import { Service } from '@/components/molecules'
 
 export default defineComponent({
   name: 'App',
-  components: { SocialNetworks }
+  components: { Service }
 })
 </script>
 
