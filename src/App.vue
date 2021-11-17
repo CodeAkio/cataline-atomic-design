a46a238db1037ab4a9921108cc23fb7cc9551f8c
CodeAkio <siqueira_2004@hotmail.com>
2021-11-17 14:33:53 -0300

    Create atom logo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <h1>Hello</h1>
+  <div class="component">
+    <Logo />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { Logo } from '@/components/atoms'
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  components: { Logo }
 })
 </script>
+
+<style lang="scss" scoped>
+.component {
+  width: 300px;
+}
+</style>
