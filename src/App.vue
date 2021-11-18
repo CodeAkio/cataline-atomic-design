d72d58db79638e0469080a1b9c854e198dfed1c7
CodeAkio <siqueira_2004@hotmail.com>
2021-11-17 21:18:56 -0300

    Add home template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,13 @@
 <template>
-  <div class="component">
-    <Container>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-        necessitatibus quam officia neque quia consequuntur quidem saepe
-        deleniti. Voluptatibus sunt unde minus tenetur consequatur dolore nulla
-        quae obcaecati odit ipsum.
-      </p>
-    </Container>
-  </div>
+  <HomeTemplate />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { Container } from '@/components/bosons'
+import { HomeTemplate } from '@/components/templates'
 
 export default defineComponent({
   name: 'App',
-  components: { Container }
+  components: { HomeTemplate }
 })
 </script>
-
-<style lang="scss" scoped>
-.component {
-  margin: 5rem;
-}
-</style>
